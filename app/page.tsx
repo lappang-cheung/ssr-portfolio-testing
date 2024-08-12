978f7fe51be03dbb00d9b680761088fca73012e1
Leo Cheung <lappangcheung@gmail.com>
2024-08-11 22:22:00 -0400

    Using the aceternity components, setting up the hero section and add the animations, plus custom components

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+import Hero from "@/components/Hero";
+
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10">
+      <div className="max-w-7xl w-full">
+        <Hero />
+      </div>
+    </main>
   )
 }
 
